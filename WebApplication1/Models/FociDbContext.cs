ef5c120bea451ff64ed58f529370507a1d65b397
Sushkoo <szilagyidominik005@gmail.com>
2024-09-16 08:45:50 +0200

    Add project files.

---
+++ WebApplication1/Models/FociDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class FociDbContext : DbContext
+    {
+
+    }
+}
---
+++ WebApplication1/Models/Meccs.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace WebApplication1.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int hazaiGol { get; set; }
+        public int vendegGol { get; set; }
+        public int hazaiGolFelidoben { get; set; }
+        public int vendegGolFelidoben { get; set; }
+        public string hazaiCsapatnev { get; set; }
+        public string vendegCsapatnev { get; set; }
+    }
+}
